679d93a8151c7fdbddb70f52e5525e863ebe9190
uuukiiiyooo <uuukiiiyooo@gmail.com>
2023-03-06 17:05:56 -0600

    Add event listener to mobile button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-menuIcon.addEventListener('click', function(){
-    
+let menuIcon = document.querySelector('.menuIcon');
+let showMenu = document.querySelector('.menu')
+
+menuIcon.addEventListener("click", function() {
+    showMenu.classList.add('.show-menu');
 })
