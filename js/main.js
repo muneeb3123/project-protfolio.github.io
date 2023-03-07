5205a76ed856429b4a0de8fa42f838ebfb4b509c
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-07 14:08:35 +0500

    add javascript to menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-let menuIcon = document.querySelector('.menuIcon');
-let showMenu = document.querySelector('.menu')
-
-menuIcon.addEventListener("click", function() {
-    showMenu.classList.add('.show-menu');
-})
+const menuIcon = document.querySelector('.menuIcon');
+const menu = document.querySelector('.menu');
+const closeIcon = document.querySelector('.closeIcon');
+const hamburger = document.querySelector('.hamburger');
+function toggleMenu() {
+  if (menu.classList.contains('show-menu')) {
+    menu.classList.remove('show-menu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('show-menu');
+    closeIcon.setAttribute('style', 'display: block !important');
+    menuIcon.style.display = 'none';
+  }
+}
+hamburger.addEventListener('click', toggleMenu);
+const menuItem = document.querySelectorAll('.menuItem');
+menuItem.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
