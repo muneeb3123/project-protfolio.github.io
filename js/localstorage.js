a838ed562e6b7794dfa4459f07cea1e7f63f5183
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-03-11 00:10:04 +0500

    hide bugs

--- js/localstorage.js
+++ js/localstorage.js
@@ -5,34 +5,35 @@ const formData = {
 };
 
 function populateStorage() {
-  formData.name = document.getElementById('user-name').value;
-  formData.email = document.getElementById('user-email').value;
-  formData.messege = document.getElementById('user-message').value;
+  formData.name = document.getElementById('#user-name').value;
+  formData.email = document.getElementById('#user-email').value;
+  formData.messege = document.getElementById('#user-message').value;
 
   localStorage.setItem('form-data', JSON.stringify(formData));
+  console.log(formData);
 }
 
 function setData() {
   let storageData = localStorage.getItem('form-data');
   storageData = JSON.parse(storageData);
-  document.getElementById('user-name').value = storageData.name;
-  document.getElementById('user-email').value = storageData.email;
-  document.getElementById('user-message').value = storageData.message;
+  document.getElementById('#user-name').value = storageData.name;
+  document.getElementById('#user-email').value = storageData.email;
+  document.getElementById('#user-message').value = storageData.message;
 }
 
 const inputList = [];
-inputList.push(document.getElementById('user-name'));
-inputList.push(document.getElementById('user-email'));
-inputList.push(document.getElementById('user-message'));
+inputList.push(document.getElementById('#user-name'));
+inputList.push(document.getElementById('#user-email'));
+inputList.push(document.getElementById('#user-message'));
 
 inputList.forEach((value) => {
   value.addEventListener('input', () => {
-    populateStorage();
+    populteStorage();
   });
 });
 
-if (!localStorage.getItem('form-data')) {
-  populateStorage();
-} else {
+if (!localStorage.getItem('form-Data')) {
   setData();
+} else {
+  populateStorage();
 }
