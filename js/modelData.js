d3dd09269853b16e7d20d25bf173b15017ca69ba
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-04-09 04:46:25 +0500

    modify

--- js/modelData.js
+++ js/modelData.js
@@ -1,25 +1,19 @@
 const projectData = [
   {
-    snapshot: 'req/myimage2_resized.png',
-    projectname: 'Tonic',
+    snapshot: 'req/myimage3.png',
+    projectname: 'Awesome-Books',
     technologies: ['HTML', 'CSS', 'JAVASCRIPT', 'Github', 'Ruby', 'Bootstrap'],
     dot: 'req/dot.png',
-    para: 'A daily selection Of privately personalized reads; no accounts or sign-ups',
-    frame: ['CANOPY', 'Back End Dev', '2015'],
+    para: 'The web application being presented here is a highly functional and user-friendly platform that empowers users to create and manage their own personal book lists. Built using a combination of HTML, CSS, and JavaScript',
+    frame: ['Muneeb', 'Back End Dev', '2015'],
     smBtnText: 'See Project',
-    popimage: 'req/myimage2_resized.png',
-    disdes: `Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text
-        ever since the 1500s, when an unknown printer took a galley of type
-        and scrambled it 1960s with the releaLorem Ipsum is simply dummy
-        text of the printing and typesetting industry. Lorem Ipsum has been
-        the industrys standard dummy text ever since the 1500s, when an
-        unknown printer took`,
+    popimage: 'req/myimagep.png',
+    disdes: `The web application being presented here is a highly functional and user-friendly platform that empowers users to create and manage their own personal book lists. Built using a combination of HTML, CSS, and JavaScript`,
     sep: 'req/sep.png',
     livelogo: 'req/live-icon.svg',
     sourcelogo: 'req/github-blue.svg',
-    livelink: 'https://chic-dango-8f4fa7.netlify.app/',
-    sourcelink: 'https://github.com/muneeb3123/TO-DO-LIST',
+    livelink: 'https://muneeb3123.github.io/awesome-Books/',
+    sourcelink: 'https://github.com/muneeb3123/awesome-Books',
   },
   {
     snapshot: 'req/multi.png',
