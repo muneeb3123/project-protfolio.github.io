905434a0c4219b7d2c2242d6dae0bc00e133dbe8
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-10-01 18:47:26 +0500

    update hero page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
