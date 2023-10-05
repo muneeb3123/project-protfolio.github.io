08264ed206e3b20baa5ce44fd100dffa92622e0a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-10-05 23:43:45 +0500

    update navbar

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
-const menuIcon = document.querySelector('.menuIcon');
-const menu = document.querySelector('.menu');
-const closeIcon = document.querySelector('.closeIcon');
-const hamburger = document.querySelector('.hamburger');
-function toggleMenu() {
-  if (menu.classList.contains('show-menu')) {
-    menu.classList.remove('show-menu');
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
-  } else {
-    menu.classList.add('show-menu');
-    closeIcon.setAttribute('style', 'display: block !important');
-    menuIcon.style.display = 'none';
-  }
-}
-hamburger.addEventListener('click', toggleMenu);
-const menuItem = document.querySelectorAll('.menuItem');
-menuItem.forEach(
-  (menuItem) => {
-    menuItem.addEventListener('click', toggleMenu);
-  },
-);
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-const form = document.querySelector('#form-contact');
-const email = document.querySelector('#user-email');
-const error = document.querySelector('#error-msg');
-form.addEventListener('submit', (event) => {
-  if (email.value.toLowerCase() !== email.value) {
-    error.innerHTML = 'Email must be in Lowercase';
-    event.preventDefault();
-    error.style.display = 'block';
-  } else {
-    error.innerHTML = '';
-    error.display.style = 'none';
-  }
-});
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+// ============================================= sticky navbar ==============================================
+let header = document.querySelector('.header-menu');
+
+header.classList.toggle('sticky', window.scrollY > 1000);
+
+// ============================================= remove toogle icon navbar when pressed ==============================================
+menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+// ============================================= toogle icon navbar ==============================================
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
