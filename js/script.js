25c43be0bba5335d85adffc05f470dafee48dd99
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-10-17 16:49:58 +0500

    modify

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars, no-undef
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Developer', 'Frontend Developer', 'Backend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
